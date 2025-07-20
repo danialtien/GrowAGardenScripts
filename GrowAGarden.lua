480d9fa558f2c1731200fec42026301ebd696f1f
danialtien <69303368+danialtien@users.noreply.github.com>
2025-07-20 13:32:53 +0700

    Update GrowAGarden.lua

--- GrowAGarden.lua
+++ GrowAGarden.lua
@@ -15,8 +15,6 @@ local playerGui = player:WaitForChild("PlayerGui")
 local screenGui = Instance.new("ScreenGui", playerGui)
 screenGui.Name = "GrowAGardenUI"
 screenGui.ResetOnSpawn = false
-screenGui.DisplayOrder = 10 -- Đặt thứ tự hiển thị cao hơn các UI khác
-screenGui.IgnoreGuiInset = true -- Bỏ qua khoảng cách của GUI
 screenGui.Enabled = true -- Bật GUI
 
 
